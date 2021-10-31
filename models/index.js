fb693ece6ac7e79da9a79834fddaf15fe1840ee1
Matt <matthiasvstephens@gmail.com>
2021-10-31 23:49:25 +0100

    Set up DB model relationships

--- models/index.js
+++ models/index.js
@@ -4,5 +4,32 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 // We will define relationships here
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Location.hasMany(Post, {
+  foreignKey: 'location_id',
+});
+Post.belongsTo(Location, {
+  foreignKey: 'location_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
 
 module.exports = { User, Location, Post, Comment };
