1a6bd914bfefbd9e23d6fa77b5143777828ff0f7
Matt <matthiasvstephens@gmail.com>
2021-11-01 15:00:24 +0100

    Create navbar

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
+const dashboard = require('./dashboard');
 const {
   redirectIfLogged,
   redirectIfNotLogged,
 } = require('../../utils/forRoutes');
 
+router.use('/dashboard', dashboard);
+
 // Post editing/deleting
 // Find the post by id. If req.session.userId is equal to the post's user_id, then render the edit page for it, otherwise redirect home - someone hacking lol
+
+router.get('*', async (req, res) => {
+  res.render('404', { loggedIn: req.session.loggedIn });
+});
 
 module.exports = router;
