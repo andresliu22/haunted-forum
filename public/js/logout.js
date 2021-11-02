ef5b011446ee47fdcd71d73b193568e4e82b4932
Matt <matthiasvstephens@gmail.com>
2021-11-02 14:58:46 +0100

    Modularized new post modal, updated styles heavily

--- public/js/logout.js
+++ public/js/logout.js
@@ -14,3 +14,23 @@ document.getElementById('logoutBtn').addEventListener('click', handleLogout);
 document.querySelector('.new-post').addEventListener('mouseover', () => {
   document.querySelectorAll('.new-post i')[0].classList.toggle('rotated');
 });
+
+// Google Places autocomplete for New Post
+const newPostInput = document.querySelector('#post-location');
+const newPostBtn = document.querySelector('#createPostBtn');
+
+const newAutocomplete = new google.maps.places.Autocomplete(newPostInput, {
+  types: ['(cities)'],
+});
+
+document.querySelector('.modal-content').addEventListener('mouseover', () => {
+  if (
+    !newPostInput.value ||
+    !newPostInput.value.includes(',') ||
+    newPostInput.value.split(' ').length < 2
+  ) {
+    newPostBtn.disabled = true;
+  } else {
+    newPostBtn.disabled = false;
+  }
+});
