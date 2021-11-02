8120a41b5cd266852b6f928f068277cf47005096
alonzofroman <alonzofroman@gmail.com>
2021-11-02 12:23:07 -0700

    Comment script created

---
+++ public/js/comment.js
@@ -0,0 +1,20 @@
+// Adds a comment from the single post page
+const addComment = async (event) => {
+    event.preventDefault();
+    document.getElementById("").disabled = true;
+
+    const body = document.querySelector("#new-comment").value.trim();
+    if (!body) {
+        alert('Fill out the comment field');
+        return;
+    }
+    const response = await fetch(`/api/post/${document.location.href[document.location.href.length-1]}/comment`, {
+        method: 'POST',
+        body: JSON.stringify({body}),
+        headers: {'Content-Type': 'application/json'}});
+
+    response.ok? document.location.reload(): alert('Failed to add comment');  
+};
+
+document.querySelector("#comment-form").addEventListener('submit', addComment);
+
