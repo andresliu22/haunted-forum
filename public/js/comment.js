ce171ac44a5a55a37da41c4dad0cdafa9d082e8d
Matt <matthiasvstephens@gmail.com>
2021-11-03 12:33:25 +0100

    Delete comment functionality, Extra security

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,46 @@
+const urlId = document.location.href.split('/')[4];
+
 // Adds a comment from the single post page
 const addComment = async (event) => {
-    event.preventDefault();
-    document.getElementById("comment-btn").disabled = true;
+  event.preventDefault();
+  document.getElementById('comment-btn').disabled = true;
 
-    const body = document.querySelector("#new-comment").value.trim();
-    if (!body) {
-        alert('Fill out the comment field');
-        return;
-    }
-    const urlId = document.location.href.split('/')[4]; 
+  const body = document.querySelector('#new-comment').value.trim();
+  if (!body) {
+    alert('Fill out the comment field');
+    document.getElementById('comment-btn').disabled = false;
+    return;
+  }
 
-    const response = await fetch(`/api/posts/${urlId}/comment`, {
-        method: 'POST',
-        body: JSON.stringify({body}),
-        headers: {'Content-Type': 'application/json'}});
+  const response = await fetch(`/api/posts/${urlId}/comment`, {
+    method: 'POST',
+    body: JSON.stringify({ body }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    response.ok? document.location.reload(): alert('Failed to add comment');
+  if (response.ok) {
+    document.location.reload();
+    return;
+  }
+  alert('Failed to add comment');
+  document.getElementById('comment-btn').disabled = false;
 };
 
-document.querySelector("#comment-form").addEventListener('submit', addComment);
+document.querySelector('#comment-form').addEventListener('submit', addComment);
 
+const deleteComment = async (e) => {
+  e.preventDefault();
+  const id = e.target.getAttribute('data-id');
+  const response = await fetch(`/api/posts/${urlId}/comment`, {
+    method: 'DELETE',
+    body: JSON.stringify({ id }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  response.ok ? document.location.reload() : alert('Failed to delete!');
+};
+
+if (document.querySelector('.delete-comment-btn')) {
+  document
+    .querySelector('.delete-comment-btn')
+    .addEventListener('click', deleteComment);
+}
