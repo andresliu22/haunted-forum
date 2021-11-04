401c06f342ca52aa1b60ddb9fc94eb49dc6419af
Andres Liu <andres.liu22@gmail.com>
2021-11-04 12:57:02 -0700

    Fix logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,50 +1,40 @@
 const handleLogout = async () => {
-  const response = await fetch("/api/users/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
 
-  response.ok
-    ? document.location.reload()
-    : alert("There was an error logging out!");
+  response.status === 200
+    ? document.location.replace('/login')
+    : alert('There was an error logging out!');
 };
 
-if (document.getElementById("logoutBtn")) {
-  document.getElementById("logoutBtn").addEventListener("click", handleLogout);
+if (document.getElementById('logoutBtn')) {
+  document.getElementById('logoutBtn').addEventListener('click', handleLogout);
 }
 
-document.querySelector(".new-post").addEventListener("mouseover", () => {
-  document.querySelectorAll(".new-post i")[0].classList.toggle("rotated");
+document.querySelector('.new-post').addEventListener('mouseover', () => {
+  document.querySelectorAll('.new-post i')[0].classList.toggle('rotated');
 });
 
 const handleVote = async (e) => {
-  const id = e.target.getAttribute("data-id");
+  const id = e.target.getAttribute('data-id');
   const pTag = e.target.nextElementSibling;
 
   const response = await fetch(`/api/posts/${id}/upvote`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
     e.target.disabled = true;
     // document.location.reload();
-    pTag.innerText = `Upvotes: ${parseInt(pTag.innerText.split(" ")[1]) + 1}`;
+    pTag.innerText = `Upvotes: ${parseInt(pTag.innerText.split(' ')[1]) + 1}`;
   } else {
-    alert("Cannot vote twice");
+    alert('Cannot vote twice');
   }
 };
 
-if (document.querySelector(".vote-btn")) {
-  document.querySelector(".vote-btn").addEventListener("click", handleVote);
-}
-
-if (document.querySelectorAll(".time-stamp")) {
-  document.querySelectorAll(".time-stamp").forEach((timeTag) => {
-    const currentValue = timeTag.innerText;
-    const date = new Date(currentValue).toLocaleDateString();
-    const time = new Date(currentValue).toLocaleTimeString();
-    timeTag.innerText = `Posted on: ${date} at ${time}`;
-    // console.log(timeTag.innerText);
-  });
+if (document.querySelector('.vote-btn')) {
+  document.querySelector('.vote-btn').addEventListener('click', handleVote);
 }
